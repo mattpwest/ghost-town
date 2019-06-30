51d8da94343d98a16b942ba59b2e4718f4880308
Matt Van Der Westhuizen <mattpwest@gmail.com>
2019-06-30 22:04:15 +0200

    Adds part 2 of the tutorial
    Rendering of multiple entities.
    Basic map rendering.

--- engine.py
+++ engine.py
@@ -1,28 +1,38 @@
 import tcod as libtcod
 from input_handlers import handle_input
+from entity import Entity
+from render import clear_all, render_all
+from game_map import GameMap
 
 
 def main():
     renderer = libtcod.RENDERER_SDL2
     screen_width = 80
     screen_height = 50
+    map_width = 80
+    map_height = 45
 
-    player_x = int(screen_width / 2)
-    player_y = int(screen_height / 2)
+    colors = {
+       'dark_wall': libtcod.Color(0, 0, 100),
+       'dark_ground': libtcod.Color(50, 50, 150)
+    }
+
+    player = Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.white)
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2 - 5), '@', libtcod.yellow)
+    entities = [player, npc]
 
     libtcod.console_set_custom_font('data/arial10x10.png', libtcod.FONT_TYPE_GRAYSCALE | libtcod.FONT_LAYOUT_TCOD)
     root = libtcod.console_init_root(screen_width, screen_height, 'Ghost Town', False, renderer, vsync=True)
     con = libtcod.console.Console(screen_width, screen_height)
 
+    game_map = GameMap(map_width, map_height)
+
     running = True
     while running:
-        con.default_fg = libtcod.white
-        con.put_char(player_x, player_y, ord('@'), libtcod.BKGND_NONE)
-
-        con.blit(root)
+        render_all(con, entities, game_map, screen_width, screen_height, colors)
         libtcod.console_flush()
 
-        libtcod.console_put_char(con, player_x, player_y, ' ', libtcod.BKGND_NONE)
+        clear_all(con, entities)
 
         action = handle_input()
 
@@ -32,8 +42,9 @@ def main():
 
         if move:
             dx, dy = move
-            player_x += dx
-            player_y += dy
+
+            if not game_map.is_blocked(player.x + dx, player.y + dy):
+                player.move(dx, dy)
 
         if leave:
             running = False
